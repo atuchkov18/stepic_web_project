0a9c6432b49cdd53af89d9c08a12b96970875fb0
atuchkov18 <atuchkov18@yandex.ru>
2022-09-29 13:32:44 +0400

    version 3.3

--- hello.py
+++ hello.py
@@ -1,10 +1,11 @@
 def app(environ, start_response):
     qs = environ['QUERY_STRING']
     ls = qs.split("&")
+    data = "\n".join(ls).encode('utf-8')
     status = '200 OK'
     response_headers = [
         ('Content-type', 'text/plain'),
         ('Content-Length', str(len(data)))
     ]
     start_response(status, response_headers)
-    return [ "\n".join(ls).encode('utf-8') ]
+    return [ data ]
